15865c429d50609a0fd1beafd8e0ee9c7b74ce89
extreme <newgalaxy401@gmail.com>
2024-06-17 14:08:49 +0300

    create simple angular form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,32 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {FormsModule} from '@angular/forms';
+ 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
+
   title = 'angular-form';
+  loginForm: any = {
+    login: 'Ivanov...',
+    email: 'ivanov@mail.ru',
+    password: 'iv23',
+   };
+
+   printForm(){
+   console.log(this.loginForm);
+   window.localStorage.setItem('login', this.loginForm.login);
+   window.localStorage.setItem('email', this.loginForm.email);
+   window.localStorage.setItem('password', this.loginForm.password);
+  
+   }
+
 }
+
